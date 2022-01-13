3737d38387c7e45d3822c9de14f429a100b93fc6
brent <brenton.j.andrews@gmail.com>
2022-01-12 20:23:15 -0700

    Started work on form input

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 // Select timer elements
 let timer_screen = document.getElementById("timer-screen");
-let sessionLength = 1 * 60; // Will take value from form later. Form value in minutes, converted to seconds.
+// let sessionLength = 1 * 60; // Will take value from form later. Form value in minutes, converted to seconds.
+let timer_started = false;
+
+timer_screen.textContent = "25:00";
 
 
 // Select timer buttons, add eventListeners.
+
+// Start button (submit and click events)
 let start_btn = document.getElementById("start-btn");
-start_btn.addEventListener("click", startTimer);
+let timer_values = document.getElementById("timer-form").elements;
+
+console.log(timer_values);
+
+start_btn.addEventListener("click", (event) => {
+    if (!timer_started) {
+        timer_started = true;
+        let timer_data = {
+            sessionLength: document.getElementById("session-length").value * 60,
+            sessionCount: document.getElementById("session-number").value,
+            shortBreak: document.getElementById("short-break-length").value * 60,
+            longBreak: document.getElementById("long-break-length").value * 60,
+        }
+        console.log(timer_data.sessionCount);
+        startTimer(timer_data.sessionLength);
+    }
+});
+
+let pause_btn = document.getElementById("pause-btn");
+pause_btn.addEventListener("click", () => {
+    console.log("do this button later!");
+});
+
+let refresh_btn = document.getElementById("refresh-btn");
+refresh_btn.addEventListener("onClick", function () {
+    window.location.reload();
+});
 
 
 // Start timer.
-function startTimer() {
+function startTimer(sessionLength) {
     let increment = setInterval( function() {
         incrementTimerDisplay(sessionLength);
         sessionLength--;
