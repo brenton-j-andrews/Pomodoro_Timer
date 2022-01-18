f3c3341d341b312562bff4044201ce4debaa5c97
brent <brenton.j.andrews@gmail.com>
2022-01-18 09:48:14 -0700

    Timer functionality complete, still need to finish page styling

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ let sessionLength = document.getElementById("session-length");
 let sessionCount = document.getElementById("session-count");
 let shortBreak = document.getElementById("short-break-length");
 let longBreak = document.getElementById("long-break-length");
+let sessionProgress = document.getElementById("session-progress");
+let studyBanner = document.getElementById("banner");
 
 // Global variables.
 let intervalID;
-let totalSessionLength;
 let alarmOn = false;
 let sound = new Audio("alarm.mp3");
+let session_counter = 1;
 
 // Start button -> start timer script.
 start_btn.addEventListener("click", function() {
-
+    studyBanner.classList.add("active");
     let timer_data = {
             sessionLength: sessionLength.value * 60,
             sessionCount: sessionCount.value,
@@ -27,16 +29,17 @@ start_btn.addEventListener("click", function() {
     let longBreakLen = timer_data.longBreak;
     let currentSession = timer_data.sessionCount * 2;
     let isBreak = false;
+    sessionProgress.textContent = `Session 1 of ${timer_data.sessionCount}`;
 
     intervalID = setInterval( function() {
 
         if (!isBreak) {
+            studyBanner.textContent = "Study Time!"
             incrementTimerDisplay(studyLen);
             studyLen--;
 
             if (studyLen === -1) {
                 alarm() 
-                console.log("time for a break!");
                 currentSession--;
                 isBreak = true;
                 studyLen = timer_data.sessionLength;
@@ -44,12 +47,14 @@ start_btn.addEventListener("click", function() {
         } 
 
         else if (isBreak && currentSession != 1) {
+            studyBanner.textContent = "Break time!"
             incrementTimerDisplay(breakLen);
             breakLen--;
 
             if (breakLen === -1) {
                 alarm() 
-                console.log("back to studying!");
+                session_counter++;
+                sessionProgress.textContent = `Session ${session_counter} of ${timer_data.sessionCount}`;
                 isBreak = false;
                 breakLen = timer_data.shortBreak;
                 currentSession--;
@@ -57,7 +62,7 @@ start_btn.addEventListener("click", function() {
         }
 
         else {
-            console.log(currentSession);
+            studyBanner.textContent = "Enjoy your long break!";
             incrementTimerDisplay(longBreakLen);
             longBreakLen--;
 
@@ -69,6 +74,7 @@ start_btn.addEventListener("click", function() {
 
         if (currentSession === 0) {
             clearInterval(intervalID);
+            studyBanner.textContent = "Nicely done! Session Complete!";
             timer_screen.textContent = "00:00";
         }
 
@@ -96,7 +102,7 @@ function refresh() {
 }
 
 
-// Sound alarm, add alarm-btn animation.
+// Sound alarm, add alarm-btn animation, update session count.
 function alarm() {
     sound.play();
     alarm_btn.classList.add("active");
