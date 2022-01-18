f45fb2db7dce9219bf14ca7bacb44b398bbf61fc
brent <brenton.j.andrews@gmail.com>
2022-01-17 17:15:07 -0700

    Timer flow refactoring in progress

--- script.js
+++ script.js
@@ -1,54 +1,94 @@
 // Select timer elements
 let timer_screen = document.getElementById("timer-screen");
-// let sessionLength = 1 * 60; // Will take value from form later. Form value in minutes, converted to seconds.
-let timer_started = false;
-
-timer_screen.textContent = "25:00";
-
-
-// Select timer buttons, add eventListeners.
-
-// Start button (submit and click events)
 let start_btn = document.getElementById("start-btn");
-let timer_values = document.getElementById("timer-form").elements;
+let sessionLength = document.getElementById("session-length");
+let sessionCount = document.getElementById("session-count");
+let shortBreak = document.getElementById("short-break-length");
+let longBreak = document.getElementById("long-break-length");
 
-console.log(timer_values);
+// Global variables.
+let intervalID;
+let totalSessionLength;
 
-start_btn.addEventListener("click", (event) => {
-    if (!timer_started) {
-        timer_started = true;
-        let timer_data = {
-            sessionLength: document.getElementById("session-length").value * 60,
-            sessionCount: document.getElementById("session-number").value,
-            shortBreak: document.getElementById("short-break-length").value * 60,
-            longBreak: document.getElementById("long-break-length").value * 60,
-        }
-        console.log(timer_data.sessionCount);
-        startTimer(timer_data.sessionLength);
+
+// Start button -> start timer script.
+start_btn.addEventListener("click", function() {
+
+    let timer_data = {
+            sessionLength: sessionLength.value * 60,
+            sessionCount: sessionCount.value,
+            shortBreak: shortBreak.value * 60,
+            longBreak: longBreak.value * 60,
     }
+
+    let studyLen = timer_data.sessionLength;
+    let breakLen = timer_data.shortBreak;
+    let longBreakLen = timer_data.longBreak;
+    let currentSession = timer_data.sessionCount * 2;
+    let isBreak = false;
+
+    intervalID = setInterval( function() {
+
+        if (!isBreak) {
+            incrementTimerDisplay(studyLen);
+            studyLen--;
+
+            if (studyLen === 0) {
+                currentSession--;
+                isBreak = true;
+                studyLen = timer_data.sessionLength;
+            }
+        } 
+
+        else if (isBreak && currentSession != 1) {
+            incrementTimerDisplay(breakLen);
+            breakLen--;
+
+            if (breakLen === 0) {
+                isBreak = false;
+                breakLen = timer_data.shortBreak;
+                currentSession--;
+            }
+        }
+
+        else {
+            console.log(currentSession);
+            incrementTimerDisplay(longBreakLen);
+            longBreakLen--;
+
+            if (longBreakLen === 0) {
+                currentSession--;
+            }
+        }
+
+        if (currentSession === 0) {
+            clearInterval(intervalID);
+            timer_screen.textContent = "00:00";
+        }
+
+    }, 1000);
 });
 
+// Pause button -> pause interval.
 let pause_btn = document.getElementById("pause-btn");
 pause_btn.addEventListener("click", () => {
-    console.log("do this button later!");
+    clearInterval(intervalID);
 });
 
+// Refresh button -> refresh page with refresh(), restore default values.
 let refresh_btn = document.getElementById("refresh-btn");
-refresh_btn.addEventListener("onClick", function () {
-    window.location.reload();
-});
+refresh_btn.addEventListener("click", refresh, false);
 
-
-// Start timer.
-function startTimer(sessionLength) {
-    let increment = setInterval( function() {
-        incrementTimerDisplay(sessionLength);
-        sessionLength--;
-    }, 1000);
-    setTimeout(() => {clearInterval(increment)}, sessionLength * 1000);
+function refresh() {
+    reload = location.reload();
+    sessionLength.value = 25;
+    sessionCount.value = 4;
+    shortBreak.value = 5;
+    longBreak.value = 30;
 }
 
-// Change timer screen display.
+
+// Update timer screen display.
 function incrementTimerDisplay(sessionLength) {
     let minutes = Math.floor(sessionLength / 60).toString();
     let seconds = (sessionLength % 60).toString();
@@ -60,4 +100,3 @@ function incrementTimerDisplay(sessionLength) {
     }
     timer_screen.textContent = `${minutes}:${seconds}`;
 }
-
